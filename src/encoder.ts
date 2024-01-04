116e8b502446ab4e06862661ca7effdcb3e6adc8
Adam Soltys <asoltys@gmail.com>
2024-01-04 09:30:13 -0800

    rm textencoder polyfill

--- src/encoder.ts
+++
@@ -1,8 +0,0 @@
-let TextEncoder;
-if (typeof globalThis.TextEncoder === 'undefined') {
-    TextEncoder = require('text-encoding').TextEncoder;
-} else {
-    TextEncoder = globalThis.TextEncoder;
-}
-
-export default TextEncoder;
